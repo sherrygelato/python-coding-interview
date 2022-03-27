01dc7300e3172d24f07fbf85615f37de15305a7b
Ford <weston0713@gmail.com>
2022-03-27 20:00:14 +0900

    Ch10 ongoing

---
+++ ch10_deque_priority_queue/ford_27_merge_k_sorted_lists.py
@@ -0,0 +1,19 @@
+"""
+CH10. 27_Merge_k_Sorted_Lists.py
+"""
+
+class ListNode:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+# 입력
+# [
+#     1->4->5,
+#     1->3->4,
+#     2->6
+# ]
+
+# 출력: 1->1->2->3->4->4->5->6
+
+# --------------------------------------------------
