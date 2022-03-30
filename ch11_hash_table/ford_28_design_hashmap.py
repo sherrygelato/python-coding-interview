3c2023ccecd69c32bc326f3a7e754a0e8162dd31
Ford <weston0713@gmail.com>
2022-03-30 16:05:04 +0900

    Ford Ch11 ongoing

---
+++ ch11_hash_table/ford_28_design_hashmap.py
@@ -0,0 +1,22 @@
+"""
+CH10. 28_Design_HashMap.py
+"""
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+
+class ListNode:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+# 입력
+
+# 출력
+
+# --------------------------------------------------
