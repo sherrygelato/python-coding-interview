ea00faa1064d5b9879dcd0e43b47f49de8e6b516
Ford <weston0713@gmail.com>
2022-04-01 14:59:11 +0900

    ford Ch12 start

---
+++ ch12_graph/ford_32_number_of_islands.py
@@ -0,0 +1,18 @@
+"""
+CH12. 32_Number_of_Islands.py
+"""
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+
+# 입력
+
+# 출력
+# [1, 2]
+
+# --------------------------------------------------
