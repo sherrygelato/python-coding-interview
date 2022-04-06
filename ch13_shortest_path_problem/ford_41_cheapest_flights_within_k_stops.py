7db48d7333c793430ba08175c796d88096032d5d
Ford <weston0713@gmail.com>
2022-04-06 09:32:01 +0900

    ford Ch13 done

--- ch13_shortest_path_problem/ford_41_cheapest_flights_within_k_stops.py
+++ ch13_shortest_path_problem/ford_41_cheapest_flights_within_k_stops.py
@@ -15,9 +15,30 @@ n = 3
 edges = [[0, 1, 100], [1, 2, 100], [0, 2, 500]]
 src = 0
 dst = 2
-k = 0
+K = 0
 
 # 출력
 output = 500
 
 # --------------------------------------------------
+def findCheapestPrice(n, flights, src, dst, K):
+    graph = collections.defaultdict(list)
+    
+    for u, v, w in flights:
+        graph[u].append((v, w))
+        
+    Q = [(0, src, K)]
+    
+    while Q:
+        price, node, k = heapq.heappop(Q)
+        if node == dst:
+            return price
+        if k >= 0:
+            for v, w in graph[node]:
+                alt = price + w
+                heapq.heappush(Q, (alt, v, k-1))
+    return -1
+
+
+print(findCheapestPrice(n, edges, src, dst, K))
+print(findCheapestPrice(n, edges, src, dst, K) == output)
