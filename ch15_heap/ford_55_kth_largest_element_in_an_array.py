24424e8a8720e4cd17d37f374ff847c041c53721
Ford <weston0713@gmail.com>
2022-04-28 22:12:39 +0900

    ford Ch15 done

---
+++ ch15_heap/ford_55_kth_largest_element_in_an_array.py
@@ -0,0 +1,68 @@
+"""
+CH15. 55_Kth_Largetst_Element_in_an_Array.py
+"""
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+
+from cv2 import FAST_FEATURE_DETECTOR_NONMAX_SUPPRESSION
+
+
+# 입력 1 
+input = [3,2,1,2,4,5,5,6]
+k = 4
+# 출력 1
+output = 4
+
+
+# --------------------------------------------------
+def findKthLargest(nums, k):
+    heap = list()
+    for n in nums:
+        heapq.heappush(heap, -n)
+        
+    for _ in range(1, k):
+        heapq.heappop(heap)
+        
+    return -heapq.heappop(heap)
+
+
+print(findKthLargest(input, k))
+print(findKthLargest(input, k) == output)
+
+
+# --------------------------------------------------
+def findKthLargest2(nums, k):
+    heapq.heapify(nums)
+    
+    for _ in range(len(nums) - k):
+        heapq.heappop(nums)
+        
+    return heapq.heappop(nums)
+
+
+print(findKthLargest2(input, k))
+print(findKthLargest2(input, k) == output)
+
+
+# --------------------------------------------------
+def findKthLargest3(nums, k):
+    return heapq.nlargest(k, nums)[-1]
+
+
+print(findKthLargest3(input, k))
+print(findKthLargest3(input, k) == output)
+
+
+# --------------------------------------------------
+def findKthLargest4(nums, k):
+    return sorted(nums, reverse=True)[k-1]
+
+
+print(findKthLargest4(input, k))
+print(findKthLargest4(input, k) == output)
